0baa166ffa648defdb8869344815b1443e41a6e4
Krista Goralczyk <krista.goralczyk@gmail.com>
2025-03-19 11:32:35 -0400

    cleanup nextconfig and ts includes

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
   eslint: { ignoreDuringBuilds: true, },
   images: { unoptimized: true },
-	experimental: {
-		serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-	},
   webpack: (config) => {
 		config.externals.push({
 			harperdb: 'commonjs harperdb',
 		});
-
-    config.resolve.alias = {
-			...config.resolve.alias,
-			"sharp$": false,
-			"onnxruntime-node$": false,
-		};
-
 		return config;
 	},
 };
