f2cade4739e6ad509e314141b85d6f1794e59ec9
Krista Goralczyk <krista.goralczyk@gmail.com>
2025-03-19 11:34:16 -0400

    rm harperdb import in resources

--- resources.js
+++ resources.js
@@ -1,13 +1,11 @@
-import { tables } from 'harperdb';
 import productdata from "./productdata.json" with { type: "json" };
-const { Product, Traits } = tables;
 
 // product table seed data
 for (const product of productdata) {
-	Product.put(product);
+	tabls.Product.put(product);
 }
 
 // trait table seed data
 // Typically this data would come from a tool like Segment, Optimizely, etc
 const USER_TRAITS = ['sporty', 'likes computers', 'lives near a ski resort'];
-Traits.put({ id: "1", traits: USER_TRAITS});
+tables.Traits.put({ id: "1", traits: USER_TRAITS});
